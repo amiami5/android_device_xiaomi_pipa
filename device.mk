53f481c1ef8ad50c5a639d4bd45be74476fae232
ChrisCatto <chriscatto@proton.me>
2024-11-21 12:16:34 +0200

    pipa: Disable game default frame rate feature
    * This feature was enabled by default on Android 15 and limits the
      refresh rate to 60FPS in some games. Disabling it allows games to use
      any refresh rate without restrictions.
    
    Change-Id: Iecea8c138c48093905318756ff670d4268d6d3b1
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- device.mk
+++ device.mk
@@ -31,13 +31,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
-# Keyboard
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/idc/Xiaomi_Keyboard.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Xiaomi_Keyboard.idc
-
-# Keylayout
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/Xiaomi_Smart_Pen_Keyboard.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Xiaomi_Smart_Pen_Keyboard.kl
+# Display
+PRODUCT_VENDOR_PROPERTIES += \
+    debug.graphics.game_default_frame_rate.disabled=1 \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
