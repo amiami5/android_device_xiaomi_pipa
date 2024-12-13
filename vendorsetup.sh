8af87b466857b81b11661d4e0756422480adc365
ami <github@amisuke.com>
2024-12-14 02:10:57 +0900

    updaterを自分向ける
    Change-Id: I057338a9c15d21479a797a86c88ba1001cb08347

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,7 @@
 echo "recovery用固有コード"
 cd bootable/recovery && git fetch https://github.com/amiami5/android_bootable_recovery.git 15 && git cherry-pick feaff1fb244afbb87792338c6a113f5650237157
 cd ../../
+
+echo "updaterを自分に向ける"
+cd packages/apps/Updater && git fetch https://github.com/amiami5/android_packages_apps_Updater.git 15.0 && git cherry-pick 18c5680d874d11d8b5950ef903c712beccf20df5
+cd ../../../
