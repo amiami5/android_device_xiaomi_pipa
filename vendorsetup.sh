afb1b234451456ebade0256a795167e1b2eea1f1
ami <github@amisuke.com>
2025-02-23 07:26:35 +0900

    Updaterのcommit変更
    Change-Id: I232e765621a2da2697af682444927cc8f0a1aebe

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,5 +5,5 @@ cd bootable/recovery && git fetch https://github.com/amiami5/android_bootable_re
 cd ../../
 
 echo "updaterを自分に向ける"
-cd packages/apps/Updater && git fetch https://github.com/amiami5/android_packages_apps_Updater.git 15.0 && git cherry-pick 18c5680d874d11d8b5950ef903c712beccf20df5
+cd packages/apps/Updater && git fetch https://github.com/amiami5/android_packages_apps_Updater.git 15.0 && git cherry-pick 7da28aae83d19bab3e39a32d27a58e3cd8c87f58
 cd ../../../
