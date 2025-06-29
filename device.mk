015a7dd0a40894dc6eeee9779fdee550b768b60d
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2025-06-29 12:03:24 +0530

    pipa: Don't build memecam for now

--- device.mk
+++ device.mk
@@ -30,9 +30,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
-# MiuiCamera
--include device/xiaomi/camera/miuicamera.mk
-
 # Display
 PRODUCT_VENDOR_PROPERTIES += \
     debug.graphics.game_default_frame_rate.disabled=1 \
