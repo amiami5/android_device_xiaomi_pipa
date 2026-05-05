566e6aa541a8ac9503c772150ce7231ebec10744
Null Pointer <nullpointer1101@outlook.com>
2026-05-05 17:27:17 +0530

    pipa: props: move debug.graphics.game_default_frame_rate.disabled to vendor.prop

--- device.mk
+++ device.mk
@@ -30,10 +30,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
-# Display
-PRODUCT_VENDOR_PROPERTIES += \
-    debug.graphics.game_default_frame_rate.disabled=1 \
-
 # MIDL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/android.hardware.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.midi.xml
