536aa4ee2f56923f5f374051a3fb3ffe7675888f
ami <github@amisuke.com>
2026-07-11 00:57:12 +0900

    新環境用に変更
    Change-Id: I378729651b0ceb9f2782fb1ca346f02dbba8473b

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if ! mountpoint -q "$HOME/axion/out"; then
-    sudo mount --bind /mnt/aosp_out "$HOME/axion/out"
-fi
+#if ! mountpoint -q "$HOME/axion/out"; then
+#    sudo mount --bind /mnt/aosp_out "$HOME/axion/out"
+#fi
 # ──────────────────────────────────────────────────────────────
 # 🎨 Terminal Colors
 # ──────────────────────────────────────────────────────────────
